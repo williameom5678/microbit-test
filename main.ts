2ed3cd9b318ea3f6218ea15c012c19a3df347893
williameom <66811147+williameom5678@users.noreply.github.com>
2020-06-23 13:11:10 +0900

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonPressed(Button.A, function () {
+    basic.showString("O")
+})
+input.onButtonPressed(Button.AB, function () {
+    basic.clearScreen()
+})
+input.onButtonPressed(Button.B, function () {
+    basic.showString("X")
+})
